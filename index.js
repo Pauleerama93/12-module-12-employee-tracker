87d8fcd8f420bad1c112d622457166de3e23a8ff
rpaulnelsonii@gmail.com <rpaulnelsonii@gmail.com>
2024-06-05 23:13:20 -0700

    aded .env file and hide the proper info

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const inquirer = require('inquirer');
 const { Pool } = require('pg');
+require('dotenv').config()
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'qwer',
-    database: 'employee_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATEBASE,
     port: 5432
+    
 },
     console.log("Connected to the database")
 );
